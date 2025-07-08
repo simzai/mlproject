b12269da0ec994aeaba4c9b2369444d57b81ad47
simran <ksimranjuneja19@gmail.com>
2025-07-08 22:08:24 +0530

    hyper parameter trained

--- src/utils.py
+++ src/utils.py
@@ -23,11 +23,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(x_train,y_train,x_test,y_test,models) :
+def evaluate_model(x_train,y_train,x_test,y_test,models,param) :
     try:
         report={}
         for i in range(len(list(models))):
             model= list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            
+            gs= GridSearchCV(model,para,cv=3)
+
+            gs.fit(x_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
             y_train_pred= model.predict(x_train)
             y_test_pred= model.predict(x_test)
