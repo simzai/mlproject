dc999108b67651b68e644ed61e1bf641834e2861
simran <ksimranjuneja19@gmail.com>
2025-07-08 21:27:58 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,3 +1,4 @@
+
 import os
 import sys
 from src.exception import CustomException
@@ -6,54 +7,59 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+
+from src.components.model_trainer import ModelTrainer
+
+# Initialize Data Ingestion Configuration
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
-    test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"data.csv") 
+    train_data_path: str = os.path.join('artifacts','train.csv')
+    test_data_path: str = os.path.join('artifacts','test.csv')
+    raw_data_path: str = os.path.join('artifacts','data.csv')
 
-
+# Create a class for Data Ingestion
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionConfig()
+        self.ingestion_config = DataIngestionConfig()
 
-        
-    def initiate_data_ingestion(self):
-        logging.info('entered the data ingestion method or component')    
+    def initate_data_ingestion(self):
+        logging.info('Data ingestion method Started')
         try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info('Dataset read as pandas Dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
             
-            df = pd.read_csv('notebook\\data\\stud.csv')
-            logging.info('read the dataset as dataframe')
+            logging.info('Train Test Split Initiated')
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index= False,header=True)
-            logging.info('train test split activated')
-
-            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-           
-         
-            logging.info('hi there')
-            logging.info(f"Saving train at: {self.ingestion_config.train_data_path}")
-
-  
-            train_set.to_csv(self.ingestion_config.train_data_path,index= False,header=True)
-            logging.info(f"About to write test CSV to {self.ingestion_config.test_data_path}")
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-
-
-            logging.info('ingestion of data is completed')
+            logging.info('Ingestion of Data is completed')
 
             return(
-                self.ingestion_config.test_data_path,
-                self.ingestion_config.train_data_path
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
             )
-        except Exception as e: 
-            raise CustomException(e,sys)
-            
 
+        except Exception as e:
+            logging.info('Exception occured at Data Ingestion stage')
+            raise CustomException(e, sys)
+    
+# Run Data ingestion
+if __name__ == '__main__':
+    obj = DataIngestion()
+    train_data, test_data = obj.initate_data_ingestion()
 
-if __name__=="__main__":
-    obj =DataIngestion()
-    obj.initiate_data_ingestion()           
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initate_data_transformation(train_data,test_data)
+
+    modeltrainer= ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
